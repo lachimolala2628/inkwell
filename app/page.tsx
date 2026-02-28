c6600df6813e54ddf8f9ae7f7f06baca38409640
Ayush Kumar <aayushkumar1228@gmail.com>
2026-03-01 02:42:03 +0530

    enhance Next.js boilerplate with custom fonts, page metadata, and update the global CSS for application

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <h1 className='text-2xl font-bold text-red-500'>hello world</h1>
+    <h1 className='text-2xl font-bold text-red-500 underline m-4'>hello world</h1>
   )
 }
 
