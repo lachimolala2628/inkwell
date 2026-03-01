a77c264f92bf574dfc3c59fcf9b70afc17a407ef
Ayush Kumar <aayushkumar1228@gmail.com>
2026-03-02 02:49:06 +0530

    create homepage layout and components

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import HeroSection from '@/components/HeroSection'
+import { sampleBooks } from '@/lib/constants'
+import BookCard from '@/components/BookCard'
 
 const page = () => {
   return (
-    <div></div>
+    <main className='wrapper container'>
+      <HeroSection />
+
+      <div className="library-books-grid">
+        {
+          sampleBooks.map((book) => (
+            <BookCard key={book._id} title={book.title} author={book.author} coverURL={book.coverURL} slug={book.slug} />
+          ))
+        }
+      </div>
+    </main>
   )
 }
 
