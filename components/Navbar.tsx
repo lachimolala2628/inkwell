337500c8fa3b2eaee403a3f74279f17a1b8fe18a
Ayush Kumar <aayushkumar1228@gmail.com>
2026-03-03 02:15:11 +0530

    implement add new book form UI, Voice selection component, File uploader UI, Loading overlay UI, and Upload schema

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = () => {
     const { user } = useUser();
 
     return (
-        <header className="w-full fixed z-50 bg-('--bg-primary')">
+        <header className="w-full fixed z-50 bg-[var(--bg-primary)]">
             <div className="wrapper navbar-height py-4 flex justify-between items-center">
                 <Link href='/' className='flex gap-0.5 items-center'>
                     <Image src='/assets/logo.png' width={42} height={26} alt='Inkwell' />
